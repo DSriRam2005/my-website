c03b70d0a1ac2808b60aabf8dc7c0437283a544a
DSriRam2005 <sdommeti95@gmail.com>
2025-02-17 22:56:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var navbar = document.getElementById("navbar");
     var backToTopButton = document.getElementById("backToTop");
-    var sticky = navbar.offsetTop;
 
     window.onscroll = function () {
-        if (window.pageYOffset >= sticky) {
-            navbar.classList.add("sticky");
-        } else {
-            navbar.classList.remove("sticky");
-        }
-
         if (window.pageYOffset > 300) {
             backToTopButton.style.display = "block";
         } else {
@@ -20,22 +12,4 @@ document.addEventListener("DOMContentLoaded", function () {
     backToTopButton.onclick = function () {
         window.scrollTo({ top: 0, behavior: "smooth" });
     };
-
-    // Form validation
-  document.getElementById("contactForm").addEventListener("submit", function(e) {
-        e.preventDefault(); // Prevent default form submission
-        
-        var formData = new FormData(this);
-
-        fetch("https://docs.google.com/forms/d/e/1FAIpQLSc3WdXJ149iawfo6ddptQpKBSJNg28ewGCBuzbKMXZR2nBrZA/formResponse", {
-            method: "POST",
-            mode: "no-cors",
-            body: formData
-        }).then(() => {
-            alert("Thank you! Your response has been submitted.");
-            document.getElementById("contactForm").reset();
-        }).catch(error => {
-            alert("There was an error submitting the form.");
-        });
-    });
-    
+});
